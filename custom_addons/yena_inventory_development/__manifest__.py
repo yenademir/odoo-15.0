caacf02744746165200132984413e515820ea5d4
yenademir <116823062+yenademir@users.noreply.github.com>
2024-01-01 15:41:12 +0300

    Update __manifest__.py

--- custom_addons/yena_inventory_development/__manifest__.py
+++ custom_addons/yena_inventory_development/__manifest__.py
@@ -2,11 +2,11 @@
     'name': "YENA Inventory Development",
     'version': '15.1.1',
     'summary': "All Development about product.template and stock.picking",
-    'author': "Alperen Alihan ER",
+    'author': "Emre MATARACI, Alperen Alihan ER, Selçuk ATAV",
     'website': "https://yenaengineering.nl",
     'category': 'Inventory',
     'license': 'LGPL-3',
-    'depends': ['product', 'stock', 'barcodes', 'digest', 'purchase', 'base', 'account', 'delivery'],
+    'depends': ['product', 'stock', 'barcodes', 'digest', 'purchase', 'base', 'account', 'delivery' , 'batch_transfer_extension'],
     'data': [
         'views/inventory_development.xml',
         'security/ir.model.access.csv',
